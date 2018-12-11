7da0ea29df6c8bccddfe12e1e6ac3ebb9a16bad7
reetou <fillmasterson@gmail.com>
2018-12-12 02:09:10 +0300

    Edit config

--- core/config.js
+++ core/config.js
@@ -3,13 +3,13 @@ const DEV = {}
 const DEBUG = process.env.NODE_ENV !== 'production'
 const AUTH = {}
 
+require('dotenv').config();
 
 if (DEBUG) {
-  require('dotenv').config();
 
   REDIS.host = process.env.REDIS_HOST
-  REDIS.name = process.env.REDIS_NAME
   REDIS.password = process.env.REDIS_PASSWORD
+  if (process.env.REDIS_PORT) REDIS.port = process.env.REDIS_PORT
 
   AUTH.GH_CLIENT_ID = process.env.GH_CLIENT_ID
   AUTH.GH_CLIENT_SECRET = process.env.GH_CLIENT_SECRET
