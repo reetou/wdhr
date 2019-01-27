9144de29d857a7167e2b21200b1fdab77c3f6c34
reetou <fillmasterson@gmail.com>
2019-01-28 02:02:15 +0300

    Refactor user

--- core/user.js
+++ core/user.js
@@ -7,11 +7,12 @@ const axios = require('axios')
 const fs = require('fs')
 const path = require('path')
 const logUserError = require('debug')('user:error')
+const UserModel = require('../models/UserModel')
+const ProjectModel = require('../models/ProjectModel')
+const PublicRepoModel = require('../models/PublicRepoModel')
+const ProjectRatingModel = require('../models/ProjectRatingModel')
 
 const USER_PUBLIC_REPOS = login => `user_${login}_public_repos`
-const USER_RATED_PROJECTS = login => `user_${login}_rated_projects`
-const USER_RATED_ARTICLES = login => `user_${login}_rated_articles`
-const USER_PROJECTS = login => `user_${login}_projects`
 
 class User {
 
@@ -21,35 +22,34 @@ class User {
 
   async get(login) {
     if (!login) return false
-    const user = await db.findInHash('users', sha1(login))
+    const user = await UserModel.query().where({ login }).first()
     if (!user) return false
-    return JSON.parse(user)
+    return user
   }
 
   async getRatedProjects(login) {
-    const rated = await db.findAllInHash(USER_RATED_PROJECTS(login))
-    return _.map(rated, v => JSON.parse(v).id)
-  }
-
-  async getRatedArticles(login) {
-    const rated = await db.findAllInHash(USER_RATED_ARTICLES(login))
-    return _.map(rated, v => JSON.parse(v).id)
+    return await ProjectRatingModel.query().where({ login }).map(p => p.project_id)
   }
 
   async getSafeUserData(login, withRatedProjects = false, withRatedArticles = false) {
     const user = await this.get(login)
     if (!user) return false
     user.rated = await this.getRatedProjects(login)
-    user.rated_articles = await this.getRatedArticles(login)
     delete user.github_id
     user.project_ownership_count = await this.getUserProjectsCount(login)
     user.public_repos = await this.getPublicRepos(login)
-    user.public_repos_names = user.public_repos.map(r => r.name)
+    user.public_repos_names = user.public_repos.map(r => r.full_name)
     return user
   }
 
+  /**
+   * Возвращает количество проектов у пользователя по логину
+   * @param login {string}
+   * @returns {Promise<number>}
+   */
   async getUserProjectsCount(login) {
-    return await db.getHashLen(USER_PROJECTS(login))
+    const projects = await ProjectModel.query().where({ owner: login })
+    return projects.length
   }
 
   async updatePublicRepos(url, login) {
@@ -77,81 +77,67 @@ class User {
       console.log(`Cannot reach url to update public repos with token ${token}`)
       return false
     }
-    const updatedRepos = await Promise.all(res.data.filter(r => r.owner.login === login && !r.fork && !r.private).map(async r => {
+    const updatedRepos = await Promise.all(res.data.filter(r => r.owner.login === login).map(async r => {
       const updated = _.cloneDeep(r)
       delete updated.owner
-      await db.addToHash(USER_PUBLIC_REPOS(login), r.id, JSON.stringify(r))
+      await PublicRepoModel.query().upsertGraph({
+        repository_id: r.id,
+        github_id: r.owner.id,
+        node_id: r.node_id,
+        full_name: r.full_name,
+        private: r.private,
+        language: r.language,
+        fork: r.fork
+      }, { insertMissing: true })
       return updated
     }))
     const currentRepos = await this.getPublicRepos(login)
-    const updatedReposNames = updatedRepos.map(r => r.name)
-    const currentReposNames = currentRepos.map(r => r.name)
+    const updatedReposNames = updatedRepos.map(r => r.repository_id)
+    const currentReposNames = currentRepos.map(r => r.repository_id)
     const reposNamesToDelete = currentReposNames.filter(v => !updatedReposNames.includes(v))
-    await Promise.all(reposNamesToDelete.map(async name => await db.removeFromHash(USER_PUBLIC_REPOS(login), name)))
-    await db.addToHash(`user_updates`, login, JSON.stringify({ date: Date.now() }))
+    return await PublicRepoModel.query().whereIn('repository_id', reposNamesToDelete).del()
   }
 
+  /**
+   * Возвращает все репозитории пользователя по логину
+   * @param login {string}
+   * @returns {Promise<[Object]>}
+   */
+  async getAllRepos(login) {
+    return await PublicRepoModel.query().select(['repository_id', 'full_name', 'language']).where({ login })
+  }
+
+  /**
+   * Возвращает все публичные репозитории-источники пользователя по логину
+   * @param login {string}
+   * @returns {Promise<[Object]>}
+   */
   async getPublicRepos(login) {
-    let repos = await db.findAllInHash(USER_PUBLIC_REPOS(login))
-    if (!repos) return []
-    return _.map(repos, JSON.parse)
+    return await PublicRepoModel.query().select(['repository_id', 'full_name', 'language']).where({ login }).andWhere({ private: false }).andWhere({ fork: false })
   }
 
-  async getPublicRepoById(login, id) {
-    let repo = await db.findInHash(USER_PUBLIC_REPOS(login), id)
-    if (!repo) {
-      console.log(`Cannot find repo by id ${id} for login ${login}`)
-      return false
-    }
-    return JSON.parse(repo)
-  }
-
+  /**
+   * Регистрирует пользователя
+   * @param data {Object}
+   * @param data.id {number} гитхаб айди
+   * @param data.avatar_url {string} ссылка на аватарку
+   * @param data.html_url {string} ссылка на гитхаб пользователя
+   * @param data.login {string} Логин на гитхабе
+   * @param data.created_at {Date} Дата регистрации на гитхабе
+   * @param data.updated_at {Date} Дата последнего обновления профиля
+   * @returns {Promise<Object>} Возвращает объект зарегистрированного пользователя
+   */
   async register(data) {
-    await db.addToHash('users', sha1(data.login), JSON.stringify({
-      login: data.login,
-      github_id: data.id,
-      avatar_url: data.avatar_url,
-      github: data.html_url,
-      approved: false,
-      mentor: false,
-      is_mentored: false,
-      github_register_date: data.created_at,
-      github_update_date: data.updated_at,
-    }))
-    await db.addToHash('nodes', sha1(data.login), JSON.stringify({
-      login: data.login,
-      github_id: data.id,
-      node_id: data.node_id,
-    }))
-    await db.addToHash('users_nodes', data.node_id, JSON.stringify({
-      login: data.login,
-      github_id: data.id,
-      avatar_url: data.avatar_url,
-      github: data.html_url,
-      approved: false,
-      mentor: false,
-      is_mentored: false,
-      github_register_date: data.created_at,
-      github_update_date: data.updated_at,
-    }))
-  }
-
-  async save(user) {
-    try {
-      let oldUser = await this.get(user.login)
-      if (!oldUser) {
-        logUserError(`Saving with no already saved user, is error??? LOGIN: ${user.login}`)
-        oldUser = {}
-      }
-      await db.addToHash('users', sha1(user.login), JSON.stringify({
-        ...oldUser,
-        ...user
-      }))
-      return true
-    } catch (e) {
-      logUserError('Error at save user', e)
-      return false
-    }
+    return await UserModel
+      .query()
+      .insert({
+        github_id: data.id,
+        login: data.login,
+        github_register_date: data.created_at,
+        github_update_date: data.updated_at,
+        avatar_url: data.avatar_url,
+        github_url: data.html_url
+      })
   }
 
 }
